f61a99154d814077b2da55b0f4d38d0d674cb9a1
Patrick Schless <patrick@plainlystated.com>
2013-02-16 01:55:45 -0600

    only sending one set of data

--- globe.js
+++ globe.js
@@ -202,9 +202,9 @@ DAT.Globe = function(container, colorFn) {
         for (i = 0; i < data.length; i += step) {
           lat = data[i];
           lng = data[i + 1];
-          size = data[i + 2];
+          //size = data[i + 2];
           color = colorFnWrapper(data,i);
-          //size = 0;
+          size = 0;
           addPoint(lat, lng, size, color, this._baseGeometry);
         }
       }
